170657edfb4756e421178e168663a5525a6cc4a0
Anders Ryden <rydena021@gmail.com>
2019-02-22 10:38:49 -0600

    debug heroku deploy

--- server/server.js
+++ server/server.js
@@ -40,16 +40,19 @@ app.use(express.static('build'));
 // App Set //
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Exprees will serve up production assets
+//   app.use(express.static('client/build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   // Express serve up index.html file if it doesn't recognize route
+//   const path = require('path');
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
+
+app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html')));
+
 
 /** Listen * */
 app.listen(PORT, () => {
